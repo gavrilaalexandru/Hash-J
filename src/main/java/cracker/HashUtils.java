dbe84f8c692391a2ad3ba54dce5f74715daca0fd
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-06-05 00:20:33 +0300

    feat: add HashUtils for hashing strings with specified algorithm

--- src/main/java/cracker/HashUtils.java
+++ src/main/java/cracker/HashUtils.java
@@ -1,4 +1,16 @@
 package cracker;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashUtils {
+    public static String hash(String password, String algorithm) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance(algorithm);
+        byte[] digest = md.digest(password.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
 }
