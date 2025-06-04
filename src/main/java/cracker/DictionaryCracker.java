bbc70532683d829c5cc0c131a7d5fc861fd09db9
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-06-05 00:17:00 +0300

    feat: implement DictionaryCracker structure and wordlist reading loop, pending hash utility integration

--- src/main/java/cracker/DictionaryCracker.java
+++ src/main/java/cracker/DictionaryCracker.java
@@ -1,4 +1,30 @@
 package cracker;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DictionaryCracker {
-}
+    private String targetHash;
+    private String algorithm;
+    private String wordlistPath;
+
+    public DictionaryCracker(String targetHash, String algorithm, String wordlistPath) {
+        this.targetHash = targetHash;
+        this.algorithm = algorithm;
+        this.wordlistPath = wordlistPath;
+    }
+
+    public void crack() {
+        try(BufferedReader br = new BufferedReader(new FileReader(wordlistPath))) {
+            String password;
+            while ((password = br.readLine()) != null) {
+                String hashedPassword;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+} // end class
