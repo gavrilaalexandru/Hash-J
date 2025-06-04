f66d86d1e501b80bd188007c46c095a98a9ef286
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-06-05 00:11:13 +0300

    feat: add initial CrackerApp CLI for user input of target hash, algorithm, and wordlist path

--- src/main/java/cracker/CrackerApp.java
+++ src/main/java/cracker/CrackerApp.java
@@ -1,7 +1,21 @@
 package cracker;
 
+import java.util.Scanner;
+
 public class CrackerApp {
     public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Target hash: ");
+            String targetHash = scanner.nextLine();
 
-    }
-}
+            System.out.print("Hash algorithm: ");
+            String algorithm = scanner.nextLine();
+
+            System.out.print("Wordlist path: ");
+            String wordlistPath = scanner.nextLine();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    } // end main method
+} // end class
