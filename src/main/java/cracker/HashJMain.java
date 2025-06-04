be4bcc62f0e69fe455e68838d0bf155066fec60f
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-06-05 00:39:37 +0300

    refactor: rename CrackerApp class to HashJMain

--- src/main/java/cracker/HashJMain.java
+++ src/main/java/cracker/HashJMain.java
@@ -2,7 +2,7 @@ package cracker;
 
 import java.util.Scanner;
 
-public class CrackerApp {
+public class HashJMain {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)) {
             System.out.print("Target hash: ");
