5a11a6af4c533d403723f69546b02b180b463247
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-06-05 11:55:04 +0300

    feat: enhance DigestProcessor to leverage HashType enum for secure hashing

--- src/main/java/hashj/utils/DigestProcessor.java
+++ src/main/java/hashj/utils/DigestProcessor.java
@@ -1,11 +1,14 @@
 package hashj.utils;
 
+import hashj.models.HashType;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class DigestProcessor {
     public static String hash(String password, String algorithm) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance(algorithm);
+        HashType algo = HashType.fromString(algorithm);
+        MessageDigest md = MessageDigest.getInstance(algo.getValue());
         byte[] digest = md.digest(password.getBytes());
         StringBuilder sb = new StringBuilder();
         for (byte b : digest) {
