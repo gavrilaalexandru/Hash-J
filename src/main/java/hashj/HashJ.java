b532b550156316f2e0b087aee3650e39e74da100
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-06-07 14:39:22 +0300

    feat: add banner, clear screen, and thread count input to HashJ main class

--- src/main/java/hashj/HashJ.java
+++ src/main/java/hashj/HashJ.java
@@ -1,24 +1,41 @@
 package hashj;
 
 import hashj.engines.BruteForceEngine;
+import hashj.models.AnsiColor;
+
 import java.util.Scanner;
 
 public class HashJ {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)) { // validating user input
-            System.out.print("Target hash: ");
+            AnsiColor.CLEAR_SCREEN.execute();
+            printBanner();
+
+            System.out.print(AnsiColor.YELLOW + "Target hash: " + AnsiColor.RESET);
             String targetHash = scanner.nextLine(); // can be any string, no restrictions
 
-            System.out.print("Hash algorithm (MD5, SHA=1, SHA-256): ");
+            System.out.print(AnsiColor.YELLOW + "Hash algorithm (MD5, SHA=1, SHA-256): " + AnsiColor.RESET);
             String algorithm = scanner.nextLine(); // should be one from a predefined list (enum)
 
-            System.out.print("Wordlist path: ");
+            System.out.print(AnsiColor.YELLOW + "Wordlist path: " + AnsiColor.RESET);
             String wordlistPath = scanner.nextLine(); // can be any string, no restrictions
 
+            System.out.println(AnsiColor.YELLOW + "Number of threads (default: " + Runtime.getRuntime().availableProcessors() + "): " + AnsiColor.RESET);
+            String threadsInput = scanner.nextLine();
+            int noThreads = threadsInput.isEmpty() ? Runtime.getRuntime().availableProcessors() : Integer.parseInt(threadsInput);
             BruteForceEngine bruteForceEngine = new BruteForceEngine(targetHash, algorithm, wordlistPath);
             bruteForceEngine.crack();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println(AnsiColor.RED + "Error: " + e.getMessage() + AnsiColor.RESET);;
         }
     } // end main method
+
+    private static void printBanner() {
+        System.out.println(AnsiColor.CYAN + """
+                ╔══════════════════════════════════════╗
+                ║              HashJ v2.0              ║
+                ║       Multithreaded Hash Cracker     ║
+                ╚══════════════════════════════════════╝
+                """ + AnsiColor.RESET);
+    }
 } // end class
